605f291426785229e7c97f0f5c0b5bed601a6da0
Xaivteev <yang3792@morris.umn.edu>
2018-10-02 16:15:17 -0500

    remove commented out code from previous version

--- src/applications/SpecificationReader.java
+++ src/applications/SpecificationReader.java
@@ -31,19 +31,13 @@ public class SpecificationReader {
 
     private void readJobSpecifications() {
         // input the jobs
-//        JobSpecification[] jobSpecifications = new JobSpecification[specification.getNumJobs()+1];
-//        for (int i=1; i <= specification.getNumJobs(); i++) {
-//            jobSpecifications[i] = new JobSpecification();
-//        }
-//        int[] thing1 = new int[specification.getNumJobs()];
-//        specification.setJobSpecification(jobSpecifications);
+
         for (int i = 1; i <= specification.getNumJobs(); i++) {
             System.out.println("Enter number of tasks for job " + i);
             int tasks = keyboard.readInteger(); // number of tasks
             if (tasks < 1)
                 throw new MyInputException(MachineShopSimulator.EACH_JOB_MUST_HAVE_AT_LEAST_1_TASK);
             specification.makeJobSpecNumtasks(i,tasks);
-//            jobSpecifications[i].setNumTasks(tasks);
 
             int[] specificationsForTasks = new int[2 * tasks + 1];
 
